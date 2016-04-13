3e94b6d840c4d0a5dd82e11a413584e8c973a95b
Alessandro Giusa <alessandrogiusa@gmail.com>
2016-04-13 18:10:13 +0200

    doc

--- src/giusa/software/parser/parameter/ParameterBean.java
+++ src/giusa/software/parser/parameter/ParameterBean.java
@@ -26,5 +26,7 @@ public interface ParameterBean {
   /* nothing to do here
    *
    * framework will work over reflections
+   *
+   * String usage(); // eventually this method will come.
    */
 }
